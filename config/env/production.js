f5a8662b6eb72846bee0bb61e928ac3de98267d6
Logan Born <lcborn4@gmail.com>
2019-08-01 16:52:23 -0400

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -269,8 +269,8 @@ module.exports = {
     * (https://sailsjs.com/docs/concepts/deployment/scaling)                   *
     *                                                                          *
     ***************************************************************************/
-    // adapter: '@sailshq/socket.io-redis',
-    // url: 'redis://user:password@bigsquid.redistogo.com:9562/databasenumber',
+ adapter: '@sailshq/connect-redis',
+    url: process.env.REDISTOGO_URL,
     //--------------------------------------------------------------------------
     // /\   OR, to avoid checking it in to version control, you might opt to
     // ||   set sensitive credentials like this using an environment variable.
